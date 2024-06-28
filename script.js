2f80fc13afec440be6a8fe950ecbeb1d95b4ccac
Ahmad-Khallaf05 <ahmadmajedkhallaf470@gmail.com>
2024-06-28 19:12:08 +0300

    project3 .home page html . css . js

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
-// login page 
-let emailInput = document.getElementsByClassName("sign-in-input")[0].value;
+function toggleMenu() {
+    const navList = document.getElementById('nav-list');
+    navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var homeLink = document.getElementById("Home");
+    var aboutLink = document.getElementById("About");
+    var servicesLink = document.getElementById("Services");
+    var contactLink = document.getElementById("Contact");
+   
+
+    function setActive(link) {
+        homeLink.classList.remove("active");
+        aboutLink.classList.remove("active");
+        servicesLink.classList.remove("active");
+        contactLink.classList.remove("active");
+        link.classList.add("active");
+    }
+
+    homeLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        setActive(homeLink);
+    });
+
+    aboutLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        setActive(aboutLink);
+    });
+
+    
+    servicesLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        setActive(servicesLink);
+    });
+
+    
+    contactLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        setActive(contactLink);
+    });
+});
